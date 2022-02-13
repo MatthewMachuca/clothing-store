93844e1cc99d1d01f73bf3390714f920b60c7d68
unknown <matthewmachuca457@gmail.com>
2022-02-12 20:32:20 -0800

    got motor working

---
+++ backend/database.py
@@ -0,0 +1,4 @@
+from model import Item
+import motor.motor_asyncio
+
+client = motor.motor_asyncio
--- backend/main.py
+++ backend/main.py
@@ -16,3 +16,23 @@ app.add_middleware(
 @app.get("/")
 def read_root():
     return {'ping'}
+
+@app.get("/api/item")
+async def get_item():
+    return 1
+
+@app.get("/api/item{id}")
+async def get_item_by_id(id):
+    return 1
+
+@app.post("/api/item")
+async def post_item(item):
+    return 1
+
+@app.put("/api/item{id}")
+async def update_item(id, data):
+    return 1
+
+@app.delete("/api/item{id}")
+async def delete_item(id):
+    return 1
